95e4cf9d0d7e07457851eab0a18900d10cfce36d
Musa Halilov <halilovmusa@gmail.com>
2019-12-12 19:27:02 +0300

    insert

--- AVL/avl_tree.hpp
+++ AVL/avl_tree.hpp
@@ -19,23 +19,23 @@ namespace mshl
     };
 
     template<class T>
-    inline uint height(node<T>* p)
+    inline uint height(node<T>* v)
     {
-        return p ? p->height : 0;
+        return v ? v->height : 0;
     }
 
     template<class T>
-    inline int diff(node<T>* p)
+    inline int diff(node<T>* v)
     {
-        return p ? height(p->left) - height(p->right) : 0;
+        return v ? height(v->right) - height(v->left) : 0;
     }
 
     template<class T>
-    inline void fix_heigth(node<T>* p)
+    inline void fix_heigth(node<T>* v)
     {
-        if (!p) return;
-        uint lefth = height(p->left), righth = height(p->right);
-        p->height = (lefth > righth ? lefth : righth) + 1;
+        if (!v) return;
+        uint lefth = height(v->left), righth = height(v->right);
+        v->height = (lefth > righth ? lefth : righth) + 1;
     }
 
     template <class T>
@@ -44,7 +44,14 @@ namespace mshl
     public:
         avl_tree() : root(nullptr) {}
 
+        node<T>* insert(T key);
+
     private:
+        void rot_left(node<T>* v);
+        void rot_right(node<T>* v);
+        void balance (node<T>* v);
+        node<T>* insert(T key, node<T>* v, node<T>* parent);
+
         node<T>* root;
     };
 }
--- AVL/avl_tree_imp.hpp
+++ AVL/avl_tree_imp.hpp
@@ -1,1 +1,85 @@
 #pragma once
+
+template<class T>
+void mshl::avl_tree::rot_left(node<T>* v)
+{
+    if (!v || !v->right) return;
+    node* r = v->right;
+    {
+        node* p = v->parent;
+        r->parent = p;
+        if (p)
+        {
+            if (p->left == v) p->left = r;
+            else p->right = r;
+        }
+    }
+    node* b = r->left;
+    r->left = v;
+    v->parent = r;
+    v->right = b;
+    if (b) b->parent = v;
+    fix_heigth<T>(v);
+    fix_heigth<T>(r);
+}
+
+template<class T>
+void mshl::avl_tree::rot_right(node<T>* v)
+{
+    if (!v || !v->left) return;
+    node* l = v->left;
+    {
+        node* p = v->parent;
+        l->parent = p;
+        if (p)
+        {
+            if (p->left == v) p->left = l;
+            else p->right = l;
+        }
+    }
+    node* b = l->right;
+    l->right = v;
+    v->parent = l;
+    v->left = b;
+    if (b) b->parent = v;
+    fix_heigth<T>(v);
+    fix_heigth<T>(l);
+}
+
+template<class T>
+void mshl::avl_tree::balance(node<T>* v)
+{
+    if (!v) return;
+    uint diff_ = diff(v);
+    if (diff_ == 2)
+    {
+        if (diff(v->right) < 0) rot_right(v->right);
+        rot_left(v);
+    }
+    else if (diff_ == -2)
+    {
+        if (diff(v->left) < 0) rot_left(v->left);
+        rot_right(v);
+    }
+}
+
+
+template<class T>
+mshl::node<T>* mshl::avl_tree::insert(T key, node<T>* v, node<T>* parent)
+{
+    if (!v) return new node(key, parent);
+    if (v->key == key) return v;
+    node* newp = nullptr;
+    if (v->key > key) {newp = (v->left = insert(key, v->left, v));}
+    else {newp = (v->right = insert(key, v->right, v));}
+    balance(v);
+    return newp;
+}
+
+
+template<class T>
+mshl::node<T>* mshl::avl_tree::insert(T key)
+{
+    node<T>* newp = insert(key, root, nullptr);
+    root = root ? root : newp;
+}
